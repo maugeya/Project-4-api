953dc14b623e5a738b5333b04857dc3363b854ef
Angela Maugey <a.maugey@gmail.com>
2017-07-23 11:40:50 +0100

    validations and authentication complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_and_belongs_to_many :topics
+
+  validates :title, presence: true, length: { in: 2..120 }
+  validates :body, presence: true, length: { in: 2..9000 }
+  validates :topics, presence: true 
 end
