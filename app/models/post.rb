14bf075ce616186ba056192f98927c2f23873b35
Jack11709 <jackmay89@live.com>
2017-07-27 10:31:06 +0100

    making users update/delete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_and_belongs_to_many :topics
 
   validates :title, presence: true, length: { in: 2..80 }
