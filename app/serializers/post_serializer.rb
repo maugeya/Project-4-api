b3a82bd9f1ae27235d7e6fae9e134bb3d905ee05
Angela Maugey <a.maugey@gmail.com>
2017-07-27 13:08:08 +0100

    styling done on indexPost

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,9 +1,13 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :image_src, :body, :link_url
+  attributes :id, :title, :image_src, :body, :link_url, :shortDescription
   has_many :comments
   has_many :topics
   belongs_to :user
   def image_src
   	object.image.url
+  end
+
+  def shortDescription
+     object.body.truncate(120)
   end
 end
